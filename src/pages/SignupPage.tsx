bd703a2bc717e731b051ca24b869b7341ca254a7
Mo Amir <mo.amir.code@gmail.com>
2023-12-27 13:19:01 +0530

    link changes in auth

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -2,16 +2,17 @@ import { ContactInputField } from "../components/contact/ContactForm"
 import { CheckBox } from "../components/filterProducts/Bike"
 import { SubmitButton } from "./SigninPage"
 import bike from "../assets/advantages/advantages_bike.jpg"
+import { Link } from "react-router-dom"
 
 
 const SignupPage = () => {
     return (
         <div className="max-w-5xl w-full mx-auto py-12 px-4" >
-            <div className="w-full flex bg-white rounded-lg shadow-lg min-h-screen" >
+            <div className="w-full flex bg-white rounded-lg shadow-lg" >
                 <div className="flex-grow max-md:hidden" >
                     <img src={bike} alt="sigin" className="object-cover h-full" />
                 </div>
-                <div className="w-[2200px] px-8 flex items-center justify-center" >
+                <div className="w-[2200px] px-8 py-16 flex items-center justify-center" >
                     <div className="flex flex-col justify-center w-full gap-12" >
                         <div className="" >
                             <h1 className="font-[Teko] font-bold text-4xl text-secondary-color max-md:text-3xl" >Welcome to <span className="text-primary-color" >Creto</span></h1>
@@ -24,7 +25,7 @@ const SignupPage = () => {
                             <ContactInputField placeHolder="Confirm Password" type="password" icon="password" />
                             <div className="flex items-center justify-between font-[Teko]" >
                                 <CheckBox id="remember" name="Remember" />
-                                <span className="hover:text-primary-color smooth_transition" >Forgot Password?</span>
+                                <Link to={'/auth/signin'} className="hover:text-secondary-color smooth_transition text-primary-color" >Have an account?</Link>
                             </div>
                             <SubmitButton name="Sign up" icon="signup" />
                         </form>
