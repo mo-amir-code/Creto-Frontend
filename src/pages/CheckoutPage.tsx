0fdd324c99abf075780621d7206362cb68ae8a7f
Mo Amir <mo.amir.code@gmail.com>
2024-01-20 13:17:54 +0530

    fixed

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -5,14 +5,20 @@ import { useAppDispatch, useAppSelector } from "../redux/hooks"
 import { selectCart } from "../redux/product/productSlice"
 import { fetchCartDataAsync } from "../redux/product/productAsyncThunk"
 import { selectLoggedInUser } from "../redux/auth/authSlice"
+import { selectUserInfo } from "../redux/user/userSlice"
+import { fetchUserAsync } from "../redux/user/userAsyncThunk"
 
 
 const CheckoutPage = () => {
     const cart = useAppSelector(selectCart);
     const loggedInUser = useAppSelector(selectLoggedInUser);
+    const userInfo = useAppSelector(selectUserInfo);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
+        if (!userInfo.data && loggedInUser) {
+            dispatch(fetchUserAsync({ userId: loggedInUser.userId }));
+        }
         if (!(cart.data.length > 0) && loggedInUser) {
             dispatch(fetchCartDataAsync({ userId: loggedInUser?.userId }));
         }
