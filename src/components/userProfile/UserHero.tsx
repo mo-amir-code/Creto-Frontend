5ef1789125266669f75fa68f9bbfff158f311453
Mo Amir <mo.amir.code@gmail.com>
2024-04-04 02:14:43 +0530

    button ui update

--- src/components/userProfile/UserHero.tsx
+++ src/components/userProfile/UserHero.tsx
@@ -4,8 +4,8 @@ import { UserInfoType } from "../../redux/user/userTypes";
 
 const UserHero = ({ userInfo }: { userInfo: UserInfoType | null }) => {
   return (
-    <div className="w-full flex items-center justify-between">
-      <div className="w-full flex items-center py-4 border-b gap-2 border-secondary-color">
+    <div className="w-full flex items-center border-b justify-between">
+      <div className="w-full flex items-center py-4 gap-2 border-secondary-color">
         {/* User image */}
         <div className="flex items-center justify-center">
           <div className="w-14 border rounded-full p-2 overflow-hidden">
@@ -23,7 +23,7 @@ const UserHero = ({ userInfo }: { userInfo: UserInfoType | null }) => {
           <h2 className="text-sm font-medium">Email: {userInfo?.email}</h2>
         </div>
       </div>
-      <button onClick={()=>handleLogOut()} className="text-lg font-bold font-[Teko]" >Logout</button>
+      <button onClick={()=>handleLogOut()} className="text-lg font-bold font-[Teko] max-sm:text-base" >Logout</button>
     </div>
   );
 };
