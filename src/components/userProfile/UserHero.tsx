948e0c6c22ce7b64880df180046792b31f620344
Mo Amir <mo.amir.code@gmail.com>
2024-04-04 02:11:59 +0530

    logout button added on user profile header

--- src/components/userProfile/UserHero.tsx
+++ src/components/userProfile/UserHero.tsx
@@ -1,24 +1,31 @@
 import pi from "../../assets/brand/logo.png";
+import { handleLogOut } from "../../layouts/HomeLayout";
 import { UserInfoType } from "../../redux/user/userTypes";
 
-const UserHero = ({userInfo}:{userInfo:UserInfoType | null}) => {
-
-    return (
-        <div className="w-full flex items-center py-4 border-b gap-2 border-secondary-color" >
-            {/* User image */}
-            <div className="flex items-center justify-center" >
-                <div className="w-14 border rounded-full p-2 overflow-hidden" >
-                    <img src={userInfo?.photoUrl || pi} alt={userInfo?.name} className="w-14" />
-                </div>
-            </div>
-
-            {/* User Info */}
-            <div className="" >
-                <h1 className="text-lg font-bold font-[Teko]" >{userInfo?.name}</h1>
-                <h2 className="text-sm font-medium" >Email: {userInfo?.email}</h2>
-            </div>
+const UserHero = ({ userInfo }: { userInfo: UserInfoType | null }) => {
+  return (
+    <div className="w-full flex items-center justify-between">
+      <div className="w-full flex items-center py-4 border-b gap-2 border-secondary-color">
+        {/* User image */}
+        <div className="flex items-center justify-center">
+          <div className="w-14 border rounded-full p-2 overflow-hidden">
+            <img
+              src={userInfo?.photoUrl || pi}
+              alt={userInfo?.name}
+              className="w-14"
+            />
+          </div>
         </div>
-    )
-}
 
-export default UserHero
+        {/* User Info */}
+        <div className="">
+          <h1 className="text-lg font-bold font-[Teko]">{userInfo?.name}</h1>
+          <h2 className="text-sm font-medium">Email: {userInfo?.email}</h2>
+        </div>
+      </div>
+      <button onClick={()=>handleLogOut()} className="text-lg font-bold font-[Teko]" >Logout</button>
+    </div>
+  );
+};
+
+export default UserHero;
